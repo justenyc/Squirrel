e3859ee96fab56576218dba73074fb411d94a51b
JCP5 <justen.chong@gmail.com>
2021-06-25 23:00:55 -0500

    Time Trial functinality completed

--- SquirrelPunk/Assets/_squirrelPunk/Code/Gameplay/Collectable.cs
+++ SquirrelPunk/Assets/_squirrelPunk/Code/Gameplay/Collectable.cs
@@ -6,11 +6,19 @@ public class Collectable : MonoBehaviour
 {
     [SerializeField] Type type;
 
+    public delegate void collect();
+    public event collect nutCollection;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<playermovement>())
         {
             Game_Manager.instance.CollectionListener(type);
+
+            if (nutCollection != null)
+            {
+                nutCollection();
+            }
             this.gameObject.SetActive(false);
         }
     }
--- SquirrelPunk/Assets/_squirrelPunk/Code/Gameplay/TimeTrial/TimeTrial.cs
+++ SquirrelPunk/Assets/_squirrelPunk/Code/Gameplay/TimeTrial/TimeTrial.cs
@@ -7,13 +7,18 @@ public class TimeTrial : MonoBehaviour
     [Header("Setup Objects")]
     [SerializeField] GameObject _clock;
     [SerializeField] GameObject _objectsToSpawn;
+    [SerializeField] GameObject _victoryAcorn;
     Transform[] _objects;
 
-    [Header("Time")]
+    [Header("Trial")]
     [SerializeField] bool _startCountdown;
     [Tooltip("Time in Seconds")] [SerializeField] float _allotedTime;
     [SerializeField] float _countdown;
-    
+    [Tooltip("The total number of collectables to be collected")]
+    [SerializeField] int _numberToCollect;
+    [Tooltip("The current number of collectables that the player has collected during the time trial. Resets if timer reaches zero")]
+    [SerializeField] int _numberCollected;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +34,26 @@ public class TimeTrial : MonoBehaviour
         else
         {
             _objects = _objectsToSpawn.GetComponentsInChildren<Transform>();
-            _objectsToSpawn.SetActive(false);
+
+            if (_objects.Length > 1)
+            {
+                _numberToCollect = _objectsToSpawn.GetComponentsInChildren<Transform>().Length - 1;
+                _objectsToSpawn.SetActive(false);
+                InitializeTrialCollectableListener();
+            }
+            else
+            {
+                Debug.LogError("There are no objects set as a child to Objects to Spawn");
+            }
+        }
+
+        if (_victoryAcorn == null)
+        {
+            Debug.LogError("The Victory Acorn is not assigned in the inspector");
+        }
+        else
+        {
+            _victoryAcorn.SetActive(false);
         }
     }
 
@@ -47,12 +71,38 @@ public class TimeTrial : MonoBehaviour
         }
     }
 
+    void InitializeTrialCollectableListener()
+    {
+        if (_objects.Length > 0)
+        {
+            foreach(Transform T in _objects)
+            {
+                Collectable newCollectable = T.GetComponent<Collectable>();
+                if (newCollectable != null)
+                {
+                    newCollectable.nutCollection += NutCollectionListener;
+                }
+            }
+        }
+    }
+
+    void NutCollectionListener()
+    {
+        _numberCollected++;
+
+        if(_numberCollected == _numberToCollect)
+        {
+            TimeTrialVictory();
+        }
+    }
+
     private void Reset()
     {
         _startCountdown = false;
         _countdown = _allotedTime;
         _clock.GetComponent<Clock>().EnableInteractions(true);
         _objectsToSpawn.SetActive(false);
+        _numberCollected = 0;
         UIManager.instance.SetTimeTrialTextActive(false);
     }
 
@@ -63,7 +113,6 @@ public class TimeTrial : MonoBehaviour
         {
             foreach (Transform t in _objects)
             {
-                Debug.Log(t.name);
                 t.gameObject.SetActive(b);
             }
         }
@@ -74,5 +123,10 @@ public class TimeTrial : MonoBehaviour
         SpawnObjects(true);
         _startCountdown = true;
         UIManager.instance.SetTimeTrialTextActive(true);
+    }
+
+    void TimeTrialVictory()
+    {
+        _victoryAcorn.SetActive(true);
     }
 }
